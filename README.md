38542d881d46dad46406753d3a4b665dfced522d
vinayasenareddykatta <vinayasenareddykatta@gmail.com>
2025-01-02 18:58:40 +0530

    removed sanity

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # myportfolio
 my dev portfolio
+//todo
--- sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "32xud2oy",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
-});
